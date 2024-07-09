bf9de76dfd6ba220e4cf0946f5c573f5512aeec7
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-09 16:08:17 -0300

    Adicionando construtores a entidade

--- src/main/java/com/lucas/planner/trip/Trip.java
+++ src/main/java/com/lucas/planner/trip/Trip.java
@@ -1,11 +1,16 @@
 package com.lucas.planner.trip;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Trip {
 
     @Id
@@ -29,4 +34,13 @@ public class Trip {
 
     @Column(name = "owner_email", nullable = false)
     private String ownerEmail;
+
+    public Trip(TripRequestPayload data){
+        this.destination = data.destination();
+        this.isConfirmed = false;
+        this.ownerEmail = data.owner_email();
+        this.ownerName = data.owner_name();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+    }
 }
---
+++ src/main/java/com/lucas/planner/trip/TripController.java
@@ -0,0 +1,2 @@
+package com.lucas.planner.trip;public class TripController {
+}
