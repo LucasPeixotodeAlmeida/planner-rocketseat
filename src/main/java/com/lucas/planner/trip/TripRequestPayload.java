a8878032a8786781049b57a8e320f9ac189f4032
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-09 16:09:11 -0300

    Criação do request

--- src/main/java/com/lucas/planner/trip/TripRequestPayload.java
+++ src/main/java/com/lucas/planner/trip/TripRequestPayload.java
@@ -1,2 +1,6 @@
-package com.lucas.planner.trip;public record TripRequestPayload() {
+package com.lucas.planner.trip;
+
+import java.util.List;
+
+public record TripRequestPayload(String destination, String starts_at, String ends_at, List<String> emails_to_invite, String owner_email, String owner_name) {
 }
