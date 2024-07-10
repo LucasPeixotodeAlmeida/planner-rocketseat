5ae7060b89d9a2a6b96b9632e24ac2c09661108f
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-10 19:04:03 -0300

    Criação do endpoint de confirmação da viagem

--- src/main/java/com/lucas/planner/trip/TripController.java
+++ src/main/java/com/lucas/planner/trip/TripController.java
@@ -54,4 +54,21 @@ public class TripController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<Trip> confirmTrip(@PathVariable UUID id){
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        if(trip.isPresent()){
+            Trip rawTrip = trip.get();
+            rawTrip.setIsConfirmed(true);
+
+            this.tripRepository.save(rawTrip);
+            this.participantService.triggerConfirmationEmailToParticipants(id);
+
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
