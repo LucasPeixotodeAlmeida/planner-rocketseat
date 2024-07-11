3e368c9413b21399bddd6997d64d4f03def13499
Lucas Peixoto <lucaspeixoto349@gmail.com>
2024-07-11 18:50:43 -0300

    Criação do participantRepository

---
+++ src/main/java/com/lucas/planner/participant/ParticipantRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+    
+}
